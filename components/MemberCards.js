caf09642bd6bcd05a1bf44fb0ddd5fbf5653610d
Yarelis Martin <yarelis0martin@gmail.com>
2024-04-13 18:58:13 -0500

    created the member home page and the membercard components file

---
+++ components/MemberCards.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MemberCards() {
+  return (
+    <div>
+      Members go here
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
-import { useAuth } from '../utils/context/authContext';
+import MemberCards from '../components/MemberCards';
 
 function Home() {
-  const { user } = useAuth();
-
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -15,11 +11,7 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+      <MemberCards />
     </div>
   );
 }
