c2752283088c511c5a64fff1d6dc21184ca6df87
Yarelis Martin <yarelis0martin@gmail.com>
2024-04-16 13:52:26 -0500

    added all teams on the team pg

--- pages/teams.js
+++ pages/teams.js
@@ -1,12 +1,30 @@
-/* import React, { useState } from 'react'; */
+import React, { useEffect, useState } from 'react';
+import { getTeams } from '../api/teamData';
 import TeamCards from '../components/TeamCards';
+import { useAuth } from '../utils/context/authContext';
 
-export default function teams() {
-/*   const [ teams, setTeams ] = useState([]); */
+export default function Teams() {
+  const [teams, setTeams] = useState([]);
+  const { user } = useAuth();
+
+  const getAllTeams = () => {
+    getTeams(user.uid).then(setTeams);
+  };
+
+  useEffect(() => {
+    getAllTeams();
+  });
 
   return (
-    <div>
-      <TeamCards />
+    <div
+      className="text-center d-flex flex-wrap justify-content-center align-content-center"
+      style={{
+        gap: '20px',
+      }}
+    >
+      {teams.map((team) => (
+        <TeamCards key={team.firebaseKey} teamObj={team} />
+      ))}
     </div>
   );
 }
