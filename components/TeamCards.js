f5e1a348e9afda13d64c4447168fa709ec484c85
Yarelis Martin <yarelis0martin@gmail.com>
2024-04-16 13:34:29 -0500

    added the team card jsx

--- components/TeamCards.js
+++ components/TeamCards.js
@@ -1,9 +1,20 @@
+import { useRouter } from 'next/router';
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-export default function TeamCards() {
+export default function TeamCards({ teamObj }) {
+  const router = useRouter();
   return (
     <div>
-      <h1>Teams</h1>
+      <Card.Img src={teamObj.logo} style={{ height: '200px', width: '200px' }} onClick={() => router.push(`/team/${teamObj.firebaseKey}`)} />
     </div>
   );
 }
+
+TeamCards.propTypes = {
+  teamObj: PropTypes.shape({
+    logo: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+};
